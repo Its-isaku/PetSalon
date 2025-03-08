c801d18fdd984e29d6aa273847a562b494de85c7
Isaku <isaialmeraz2003@gmail.com>
2025-03-08 07:58:22 -0800

    assigment 1

--- assigment-1/scripts/index.js
+++ assigment-1/scripts/index.js
@@ -1,4 +1,16 @@
-//? navbar
+//? Input fields
+let petName = document.getElementById('petName');
+let petAge = document.getElementById('petAge');
+let petGender = document.getElementById('petGender');
+let petSpecies = document.getElementById('petSpecies');
+let petService = document.getElementById('petService');
+let petBreed = document.getElementById('petBreed');
+let registerButton = document.getElementById('register');
+
+//? Pets array
+let pets = [];
+
+//? Navbar scroll effect
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.header-container');
     if (window.scrollY > 0) {
@@ -8,8 +20,8 @@ window.addEventListener('scroll', function() {
     }
 });
 
-//? pet constructor
-function Pet(name, age, gender, species, service, breed){
+//? Pet constructor
+function Pet(name, age, gender, species, service, breed) {
     this.name = name;
     this.age = age;
     this.gender = gender;
@@ -18,28 +30,13 @@ function Pet(name, age, gender, species, service, breed){
     this.breed = breed;
 }
 
-//? pets Array
-let pets = [];
-
-//? create 3 pets
-let Pet1 = new Pet("Bella", 6, "Femele", "Cat", "Grooming", "American Shorthair");
-pets.push(Pet1);
-
-let Pet2 = new Pet("Kira",6, "Femele", "Cat", "Grooming", "Siamis");
-pets.push(Pet2);
-
-let Pet3 = new Pet("Chipi", 5, "Femele", "Cat", "Grooming", "Taby");
-pets.push(Pet3);
-
-document.addEventListener("DOMContentLoaded", function() {
-    display();
-});
-
-//? function to display pets in cards
-function display(){
+//? Function to display pets in cards
+function display() {
     let card = document.getElementById("card");
+    if (!card) return; 
+
     card.innerHTML = "";
-    for(let i = 0; i < pets.length; i++){
+    for (let i = 0; i < pets.length; i++) {
         card.innerHTML += `
         <div class="card-item">
             <div class="card-content">
@@ -49,9 +46,70 @@ function display(){
                 <p class="card-description">Species: ${pets[i].species}</p>
                 <p class="card-description">Service: ${pets[i].service}</p>
                 <p class="card-description">Breed: ${pets[i].breed}</p>
-                ${pets[i].species === "Cat" ? '<img src="img/cat-icon.png" class="species-img">' : pets[i].species === "Dog" ? '<img src="img/dog-icon.png" class="species-img">' : ''}
+                ${pets[i].species === "Cat" ? '<img src="img/cat-icon.png" class="species-img">' 
+                : pets[i].species === "Dog" ? '<img src="img/dog-icon.png" class="species-img">' : ''}
             </div> 
         </div>
-        ` 
+        `;
     }
 }
+
+//? Register function
+function register(event) {
+    event.preventDefault(); 
+
+    //? Check if all fields are filled out
+    if (!petName.value || !petAge.value || !petGender.value || !petSpecies.value || !petService.value || !petBreed.value)
+        alert("Please fill out all fields");
+    else{  //? Create new pet object
+        let newPet = new Pet(petName.value, petAge.value, petGender.value, petSpecies.value, petService.value, petBreed.value);``
+        //? Add new pet to pets array and store in local storage
+        let storedPets = localStorage.getItem("pets");
+        pets = storedPets ? JSON.parse(storedPets) : [];
+        
+        pets.push(newPet);
+        localStorage.setItem("pets", JSON.stringify(pets)); 
+        
+        //? Clear the form
+        clearForm();
+
+        //? take user to index page
+        window.location.href = "index.html"; 
+    }
+}
+
+//? Function to clear the form
+function clearForm() {
+    petName.value = "";
+    petAge.value = "";
+    petGender.value = "";
+    petSpecies.value = "";
+    petService.value = "";
+    petBreed.value = "";
+}
+
+//? Event listener for register button
+if (registerButton) {
+    registerButton.addEventListener('click', register);
+}
+
+//? Init function
+function init() {
+    //? Get pets from local storage
+    let storedPets = localStorage.getItem("pets");
+    pets = storedPets ? JSON.parse(storedPets) : [];
+
+    //? Add default pets if there are no pets in local storage
+    if (pets.length === 0) {
+        pets.push(new Pet("Bella", 6, "Female", "Cat", "Grooming", "American Shorthair"));
+        pets.push(new Pet("Kira", 6, "Female", "Cat", "Grooming", "Siamese"));
+        pets.push(new Pet("Chipi", 5, "Female", "Cat", "Grooming", "Tabby"));
+        localStorage.setItem("pets", JSON.stringify(pets)); //? Store default pets in local storage
+    }
+
+    //? Display pets
+    display();
+}
+
+//? Call init function
+window.onload = init;
